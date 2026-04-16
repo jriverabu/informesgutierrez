cd619466c11e03a34a112a4d99d6ab5dbf3c30ea
Juan David Rivera <juanriverabu@gmail.com>
2026-04-16 09:35:59 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+/**
+ * Este archivo es el "cerebro" que arranca la aplicación.
+ * Se encarga de renderizar el componente principal (App.jsx)
+ * dentro del elemento con id 'root' definido en tu index.html.
+ */
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
